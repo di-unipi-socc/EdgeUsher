38640322d3fad5a78acc24cf853477477f54eb01
Stefano <stefano.forti92@gmail.com>
2019-03-17 17:02:34 +0100

    fixed path bug

--- nfogvopti.pl
+++ nfogvopti.pl
@@ -13,16 +13,34 @@ placement2([C|Cs], [p(C,N)|Ps], [a(N, HReqs)|As], B) :-
     service(C, HReqs, TReqs),
     forall(member(T, TReqs), member(T, TCaps)), 
     placement2(Cs, Ps, As, B),
+    latencySupport(C, N, Ps),
     enoughHardware(N, HReqs, HCaps, As).
+
+    %forall(flow(S1, S, LReq, BReq), flowSupport(S1, N, LReq, BReq, Ps)).
+    %forall(flow(S, S1, LReq, BReq), flowSupport(S, N, LReq, BReq, Ps)).
+
+latencySupport(C, N, P) :-
+    findall( l( p(C1, M), link2(N, M, L, B)), (flow(C, C1, LReq, BReq), member(p(C1,M), P), link2(N, M, L, B)) , L),
+    writenl(L).
+    %forall( member( l(p(C1,M), LReq ), L ), ), 
+   
+
+flowSupport(S1, N, LReq, BReq, P) :-
+    writenl("HEREEEE"),
+    findall(p(S1,M), member(p(S1, M), P), T), 
+    writenl(T),
+    link2(M, N, L, B),
+    L =< LReq, 
+    B >= BReq.
     %TODO: Enough Bandwidth :)
 
 query(placement2([s1, s2, s3], P, A, B)).
 
 
-link2(X, X, 0, 10000). %
-link2(X, Y, L, B) :-
+link2(X, X, 0, 10000, []). %
+link2(X, Y, L, B, [p(X,Z)|Path]) :-
     link(X, Z, Lxz, Bxz),
-    link(Z, Y, Lzy, Bzy),
+    link2(Z, Y, Lzy, Bzy),
     L is Lxz + Lzy,
     B is min(Bxz, Bzy).
 
@@ -35,11 +53,11 @@ chain(c1, [s1, s2]).
 service(s1, 3, [thing1]).
 service(s2, 3, [thing2]).
 service(s3, 3, []).
-flow(s1, s2, 30, 4).
+flow(s1, s2, 300, 4).
 flow(s2, s3, 100, 2).
 
 node(n, opA, 8, [thing1]).
-node(m, opB, 4, [thing2]).
+node(m, opB, 8, [thing2]).
 node(cloud, opC, 100000, []).
 
 link(n, m, 10, 12).
