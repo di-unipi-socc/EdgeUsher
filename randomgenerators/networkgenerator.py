d72b408b7fc7d617fe690ddad9049dc7fd0d185c
teto1992 <stefano.forti92@gmail.com>
2019-03-18 15:53:00 +0100

    graph drawing

--- randomgenerators/networkgenerator.py
+++ randomgenerators/networkgenerator.py
@@ -22,7 +22,7 @@ def createNetwork(N, p, Ops, Things, seed = 0):
     
     print(G.nodes().data())
     print(G.edges().data())
-    nx.draw(G, with_labels=True)
+    nx.draw_networkx(G, with_labels=True)
     plt.show()
     return nodes, links, G
 
