38897ea8fb5255c984a6e8daf5ee240010523d07
teto1992 <stefano.forti92@gmail.com>
2019-03-20 10:25:11 +0100

    little performance improvement on checkHardware

--- s1.pl
+++ s1.pl
@@ -22,14 +22,10 @@ place(C, P, L) :-
 %%%%%%%%%%%%%%%%%%%%%%%%%
 
 
-
 placeFlows([], P, []).
 placeFlows([f(N,M,LReq,BReq)|Fs], P, [A1|Alloc]):-
     findpath(N,M,LReq,BReq,A1),
     placeFlows(Fs, P, Alloc).
-
-
-
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -42,7 +38,6 @@ placeFlows([f(N,M,LReq,BReq)|Fs], P, [A1|Alloc]):-
 % LReq, BReq latenza e banda richiesti sul percorso,
 % Flow taccuino per ricordare i flussi mappati su un certo link.
 
-%findpath(X,Y,Path,L,B,LReq,BReq,Flow) :-
 findpath(X,Y,LReq,BReq,Flow) :-
     path(X,Y,[X],Q,L,B,LReq,BReq,Flow,4), %%% arrives at depth 4 
     reverse(Q,Path),
@@ -84,9 +79,6 @@ sumflows(X,Z,[f(A,B,BReq)|Flow],Sum):-
     B \== Z,
     sumflows(X,Z,Flow,Sum).
     
-
-
-
 %%%%%%%%%%%%%%%%%%%%%%%%%
 %% Placement of Services
 %%%%%%%%%%%%%%%%%%%%%%%%%
@@ -95,6 +87,7 @@ placeServices([S|Ss], [p(S,N)|P], Alloc) :-
     service(S, HReqs, TReqs),
     node(N, OpN, HCaps, TCaps),
     subset(TReqs, TCaps),
+    HCaps >= HReqs,
     checkHardware(N, HCaps, [a(N, HReqs)|Alloc]),
     placeServices(Ss, P, [a(N, HReqs)|Alloc]).
 
@@ -114,14 +107,103 @@ service(c, 3, []).
 flow(a,b,150,1).
 flow(a,c,100,1).
 
-node(n, op, 10, [t2]).
-node(m, op, 10, [t1]).
-node(v, op, 100, []).
+node(edge0, OpB, 23, []).
+node(cloud1, OpA, 10000, []).
+node(edge2, OpA, 46, []).
+node(edge3, OpA, 39, []).
+node(edge4, OpB, 27, [t1]).
+node(edge5, OpA, 17, []).
+node(edge6, OpA, 49, []).
+node(edge7, OpA, 5, []).
+node(edge8, OpA, 12, []).
+node(edge9, OpB, 9, []).
+node(edge10, OpB, 26, []).
+node(edge11, OpA, 8, []).
+node(edge12, OpA, 22, []).
+node(edge13, OpA, 50, []).
+node(edge14, OpB, 22, []).
+node(edge15, OpB, 5, []).
+node(edge16, OpB, 15, []).
+node(edge17, OpA, 47, []).
+node(cloud18, OpB, 10000, []).
+node(edge19, OpB, 18, []).
+node(edge20, OpA, 9, []).
+node(edge21, OpA, 5, []).
+node(edge22, OpA, 34, []).
+node(edge23, OpB, 50, []).
+node(edge24, OpA, 26, []).
+node(edge25, OpA, 5, []).
+node(edge26, OpA, 11, []).
+node(cloud27, OpB, 10000, []).
+node(edge28, OpB, 27, []).
+node(edge29, OpB, 40, []).
+link(edge0, edge28, 100, 23).
+link(edge28, edge0, 136, 11).
+link(cloud1, edge4, 29, 27).
+link(edge4, cloud1, 140, 45).
+link(cloud1, edge17, 136, 1).
+link(edge17, cloud1, 59, 45).
+link(edge2, edge6, 53, 22).
+link(edge6, edge2, 86, 6).
+link(edge2, edge25, 142, 15).
+link(edge25, edge2, 56, 27).
+link(edge3, edge8, 72, 33).
+link(edge8, edge3, 19, 41).
+link(edge3, edge11, 139, 43).
+link(edge11, edge3, 102, 16).
+link(edge4, edge21, 13, 11).
+link(edge21, edge4, 139, 16).
+link(edge5, edge15, 59, 33).
+link(edge15, edge5, 49, 43).
+link(edge5, edge21, 138, 13).
+link(edge21, edge5, 139, 38).
+link(edge5, edge23, 76, 4).
+link(edge23, edge5, 150, 23).
+link(edge6, edge19, 118, 41).
+link(edge19, edge6, 46, 24).
+link(edge7, edge20, 123, 13).
+link(edge20, edge7, 127, 9).
+link(edge8, edge9, 113, 30).
+link(edge9, edge8, 32, 48).
+link(edge10, edge19, 104, 26).
+link(edge19, edge10, 138, 15).
+link(edge10, edge24, 54, 48).
+link(edge24, edge10, 123, 31).
+link(edge11, edge13, 69, 28).
+link(edge13, edge11, 133, 28).
+link(edge11, edge24, 75, 14).
+link(edge24, edge11, 119, 22).
+link(edge12, edge15, 97, 28).
+link(edge15, edge12, 94, 26).
+link(edge12, edge16, 81, 35).
+link(edge16, edge12, 92, 15).
+link(edge12, edge19, 58, 6).
+link(edge19, edge12, 79, 33).
+link(edge13, edge22, 145, 12).
+link(edge22, edge13, 55, 46).
+link(edge13, cloud27, 81, 42).
+link(cloud27, edge13, 141, 36).
+link(edge14, edge15, 18, 12).
+link(edge15, edge14, 143, 10).
+link(edge15, edge26, 112, 48).
+link(edge26, edge15, 111, 50).
+link(edge17, edge28, 53, 3).
+link(edge28, edge17, 129, 10).
+link(cloud18, edge29, 37, 5).
+link(edge29, cloud18, 21, 28).
+link(edge19, edge20, 44, 50).
+link(edge20, edge19, 82, 31).
+link(edge23, edge28, 31, 27).
+link(edge28, edge23, 18, 9).
+link(edge25, edge28, 139, 46).
+link(edge28, edge25, 28, 41).
+link(edge26, cloud27, 28, 16).
+link(cloud27, edge26, 37, 50).
+link(cloud27, edge28, 98, 50).
+link(edge28, cloud27, 61, 33).
+link(edge28, edge29, 15, 29).
+link(edge29, edge28, 57, 15).
 
-link(n,m, 1, 2).
-link(m,n, 1, 2).
-link(v,n, 50, 2).
-link(n,v, 50, 1).
 
 
 
