f4fbc86bc17f42127cbf8067df3651cbd32b9bd4
teto1992 <stefano.forti92@gmail.com>
2019-03-27 09:46:28 +0100

    Included service processing times and multiple maxLatency constraints

--- new/w3.pl
+++ new/w3.pl
@@ -1,11 +1,11 @@
 :- use_module(library(lists)).
-:- consult('test3').
+:- consult('test4').
 query(place(C,P,L)).
     
 place(Chain, Placement, ServiceRoutes) :-
     chain(Chain, Services),
     placeServices(Services, Placement),
-    findall(f(S1, S2, Lr, Br), flow(S1, S2, Lr, Br), ServiceFlows),
+    findall(f(S1, S2, Br), flow(S1, S2, Br), ServiceFlows),
     placeFlows(ServiceFlows, Placement, ServiceRoutes),
     writenl(Placement),
     writenl(ServiceRoutes).
@@ -15,7 +15,7 @@ placeServices(Services, Placement) :-
 
 placeServices([], [], _).
 placeServices([S|Ss], [on(S,N)|P], AllocatedHW) :-
-    service(S, HW_Reqs, T_Reqs),
+    service(S, HW_Reqs, T_Reqs, T_Proc),
     node(N, OpN, HW_Caps, T_Caps),
     checkThingReqs(T_Reqs, T_Caps),
     HW_Reqs =< HW_Caps,
@@ -36,7 +36,7 @@ checkHWReqs(HW_Reqs, N, HW_Caps, [(N1,A1)|As], [(N1,A1)|NewAs]) :-
 placeFlows(ServiceFlows, Placement, ServiceRoutes) :-
     placeFlows(ServiceFlows, Placement, [], ServiceRoutes, S2SLatencies),
  %   writenl(S2SLatencies),
-    checkLatencies(S2SLatencies).
+    checkMaxLatencies(S2SLatencies).
 
 placeFlows([], _, ServiceRoutes, ServiceRoutes,[]).
 placeFlows([SF|SFs], Placement, ServiceRoutes, NewServiceRoutes, [SFLatency|S2SLatencies]) :-
@@ -46,43 +46,47 @@ placeFlows([SF|SFs], Placement, ServiceRoutes, NewServiceRoutes, [SFLatency|S2SL
 
 %assumo per ora che ci sia una sola maxlatency/2 p.e. maxlatency([s1,s2,s3],4).
 
-checkLatencies(S2SLatencies):-
-    maxlatency(Chain, RequiredLatency), 
+checkMaxLatencies(S2SLatencies):-
+    forall(maxlatency(Chain, RequiredLatency), checkLatencies(Chain, RequiredLatency, S2SLatencies)).
+
+checkLatencies(Chain, RequiredLatency, S2SLatencies):-
+    %maxlatency(Chain, RequiredLatency), 
     computeLatency(Chain, S2SLatencies, FeaturedLatency),
     write('Chain latency computed: '), writenl(FeaturedLatency),
     FeaturedLatency =< RequiredLatency,
     writenl('OK').
 
-computeLatency([S1], _, 0).
+computeLatency([S], _, 0). %TODO: Capire se vogliamo considerare T_Proc anche per l'ultimo servizio!
 computeLatency([S1,S2|RestOfChain], S2SLatencies, FeaturedLatency) :-
     member(s2slat(S1,S2,Lf), S2SLatencies),
+    service(S1, _, _, T_Proc),
     computeLatency([S2|RestOfChain], S2SLatencies, FeaturedLatency2),
-    FeaturedLatency is Lf+FeaturedLatency2.
+    FeaturedLatency is Lf+T_Proc+FeaturedLatency2.
 
-findPath(f(S1, S2, Lr, Br), Placement, ServiceRoutes, ServiceRoutes, s2slat(S1,S2,0)) :-
+findPath(f(S1, S2, Br), Placement, ServiceRoutes, ServiceRoutes, s2slat(S1,S2,0)) :-
     subset([on(S1,N),on(S2,N)], Placement).
 
-findPath(f(S1, S2, Lr, Br), Placement, ServiceRoutes, NewServiceRoutes, s2slat(S1,S2,PathLatency)) :-
+findPath(f(S1, S2, Br), Placement, ServiceRoutes, NewServiceRoutes, s2slat(S1,S2,PathLatency)) :-
     subset([on(S1,N1),on(S2,N2)], Placement),
     N1 \== N2,
-    path(N1, N2, 3, [], f(S1, S2, Lr, Br), Placement, PathLatency, ServiceRoutes, NewServiceRoutes).
+    path(N1, N2, 3, [], f(S1, S2, Br), Placement, PathLatency, ServiceRoutes, NewServiceRoutes).
 %    PathLatency =< Lr.
 
-path(N1, N2, Radius, VisitedNodes, f(S1, S2, Lr, Br), Placement, Lf, ServiceRoutes, NewServiceRoutes) :-
+path(N1, N2, Radius, VisitedNodes, f(S1, S2, Br), Placement, Lf, ServiceRoutes, NewServiceRoutes) :-
     Radius > 0,
     link(N1, N2, Lf, Bf),
  %   Lf =< Lr,
     Bf >= Br,
     update(N1, N2, Bf, S1, S2, Br, ServiceRoutes, NewServiceRoutes).
 
-path(N1, N2, Radius, VisitedNodes, f(S1, S2, Lr, Br), Placement, PathLatency, ServiceRoutes, NewServiceRoutes) :-
+path(N1, N2, Radius, VisitedNodes, f(S1, S2, Br), Placement, PathLatency, ServiceRoutes, NewServiceRoutes) :-
     Radius > 0,
     link(N1, N3, Lf, Bf), N3 \== N2, \+ member(N3, VisitedNodes),
  %   Lf =< Lr,
     Bf >= Br,
     update(N1, N3, Bf, S1, S2, Br, ServiceRoutes, ServiceRoutes2),
     NewRadius is Radius-1,
-    path(N3, N2, NewRadius, [N3|VisitedNodes], f(S1, S2, Lr, Br), Placement, PathLatency2, ServiceRoutes2, NewServiceRoutes),
+    path(N3, N2, NewRadius, [N3|VisitedNodes], f(S1, S2, Br), Placement, PathLatency2, ServiceRoutes2, NewServiceRoutes),
     PathLatency is Lf+PathLatency2.
 
 update(N1, N2, _, S1, S2, Br, [], [(N1, N2, Br,[(S1,S2)])]).
@@ -93,3 +97,10 @@ update(N1, N2, Bf, S1, S2, Br, [(X, Y, Ba, L)|ServiceRoutes], [(X, Y, Ba, L)|New
     N1 \== X,
     N2 \== Y,
     update(N1, N2, Bf, S1, S2, Br, ServiceRoutes, NewServiceRoutes).
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%% UTILS %%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%
+forall(A, B) :- \+((A,\+B)).
