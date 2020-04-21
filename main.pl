af758f7af489b85db3b2a4166e975f2fa2034e0c
Stefano <stefano.forti92@gmail.com>
2020-04-21 15:26:56 +0200

    added main

---
+++ main.pl
@@ -0,0 +1,8 @@
+:- consult('chains/chain.pl').
+:- consult('infra/full_probs.pl').
+
+:- consult('edgeusher.pl').
+query(placement(Chain, Placement, ServiceRoutes)).
+
+%:- consult('hedgeusher.pl').
+%query(placement(Chain, Placement, ServiceRoutes, 0.8, 0.8)).
